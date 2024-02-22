91cc5ebf710a04f2dac6b35f8002a85b9b2fc76b
unknown <hardeepsinh.jenya@gmail.com>
2024-02-22 12:46:25 +0530

    users api

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import './App.css'
 import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
 const App = () => {
-  const [items, setItems] = useState([])
-  const [err, setErr] = useState('')
+  const [myData, setMyData] = useState([])
   const apiData = async () => {
     try {
-      const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
-      setItems(res.data)
-    } catch (err) {
-      setErr(err.message)
+      const res = await axios.get('https://jsonplaceholder.typicode.com/users/')
+      setMyData(res.data)
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
     }
   }
   useEffect(() => {
     apiData()
   }, [])
   return (
-    <>
-      <h1>Hello Axios...</h1>
-      {err !== 0 && <h2>{err.message}</h2>}
-      <div className='table-container'>
-        <table>
-          <thead>
-            <tr>
-              <th>No.</th>
-              <th>Title</th>
-              <th>Description</th>
-            </tr>
-          </thead>
-          
-          <tbody>
-          {items.map((item) => (
-              <tr key={item.id}>
-                <td>{item.id}</td>
-                <td>{item.title.slice(0,25)}</td>
-                <td>{item.body.slice(0,139)}</td>
-              </tr>
-            ))}
-            </tbody>
-          
-        </table>
-      </div>
-    </>
+    <div>
+      {
+        myData.map(({ username, name, id, email, phone, website }) => (
+          <div className='parent' style={{ border: '1px solid #ccc', padding: '10px', margin: '10px', borderRadius: '5px', backgroundColor: '#f9f9f9' }} key={id}>
+            <h3 style={{ color: '#333', marginBottom: '5px' }}>{name}</h3>
+            <h4 style={{ color: '#666', marginBottom: '5px' }}>{username}</h4>
+            <p  style={{ color: '#888', marginBottom: '3px' }} >{email}</p>
+            <p  style={{ color: '#888', marginBottom: '3px' }}>{phone}</p>
+            <p  style={{ color: '#888', marginBottom: '3px' }}>{website}</p>
+          </div>
+        ))
+      }
+    </div>
   )
 }
+
 export default App
