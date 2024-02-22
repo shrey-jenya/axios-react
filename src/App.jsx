cdb3dd17e6106d0e3d0837fcb6b8d725c21a98e2
unknown <hardeepsinh.jenya@gmail.com>
2024-02-22 16:00:36 +0530

    meals api with search query

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
   const [meals, setMeals] = useState([])
   const [loading, setLoading] = useState(true)
   const [isError, setIsError] = useState('')
+  const [searchQuery, setSearchQuery] = useState('')
   const mealsApi = async () => {
     try {
       const response = await axios.get('https://www.themealdb.com/api/json/v1/1/filter.php?a=American')
@@ -20,15 +21,24 @@ const App = () => {
   useEffect(() => {
     mealsApi()
   }, [])
+  // ! Handle Search
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value)
+  }
+  // ! filter meals 
+  const filteredMeals = meals.filter((meal) =>
+    meal.strMeal.toLowerCase().includes(searchQuery.toLowerCase())
+  );
   return (
     <>
       <h1>Meals API - American</h1>
-      <input type="search" placeholder='search...' />
+      <input onChange={handleSearchChange} value={searchQuery} type="search" placeholder='search...' />
       {isError !== '' && <h2>Error - {isError}</h2>}
       <div className='grid'>
         {
           loading ? <h1>Loading...</h1> :
-            meals.map(({ idMeal, strMeal, strMealThumb }) => (
+          filteredMeals.length===0 ? <h2>No Meals Found</h2> :
+            filteredMeals.map(({ idMeal, strMeal, strMealThumb }) => (
               <div className='card' key={idMeal}>
                 <h4>{strMeal}</h4>
                 <img className='meals-img' src={strMealThumb} alt={strMeal} />
