97b9d4cd46c40ef4e95869bc03caeb77ec03514e
unknown <hardeepsinh.jenya@gmail.com>
2024-02-22 13:33:34 +0530

    loading added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@ import React, { useEffect, useState } from 'react'
 
 const App = () => {
   const [myData, setMyData] = useState([])
+  const [isError, setIsError] = useState('')
+  const [loading , setIsLoading] = useState(true)
   const apiData = async () => {
     try {
       const res = await axios.get('https://jsonplaceholder.typicode.com/users/')
       setMyData(res.data)
-      console.log(res.data);
+      setIsError(false)
+      setIsLoading(false)
     } catch (error) {
-      console.log(error);
+      setIsError(error.message  )
+      setIsLoading(false)
+
     }
   }
   useEffect(() => {
@@ -17,7 +22,10 @@ const App = () => {
   }, [])
   return (
     <div>
+      <h1>PlaceHolder API - USERS</h1>
+      {isError && <h1>{isError}</h1>}
       {
+        loading ? <h1>Loading...</h1> :(
         myData.map(({ username, name, id, email, phone, website }) => (
           <div className='parent' style={{ border: '1px solid #ccc', padding: '10px', margin: '10px', borderRadius: '5px', backgroundColor: '#f9f9f9' }} key={id}>
             <h3 style={{ color: '#333', marginBottom: '5px' }}>{name}</h3>
@@ -27,7 +35,7 @@ const App = () => {
             <p  style={{ color: '#888', marginBottom: '3px' }}>{website}</p>
           </div>
         ))
-      }
+     ) }
     </div>
   )
 }
