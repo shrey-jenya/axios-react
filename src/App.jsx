d6f53b2c3de73fd0a4333b5eb45666c70fbfd5b9
unknown <hardeepsinh.jenya@gmail.com>
2024-02-22 14:04:18 +0530

    loading added

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,18 @@ import axios from 'axios'
 const App = () => {
   const [items, setItems] = useState([])
   const [isError, setIsError] = useState('')
+  const [loading,setLoading]=useState(true)
   const apiData = async () => {
     try {
       const res = await axios.get(`https://jsonplaceholder.typicode.com/todos/`)
       setItems(res.data)
       console.log(res.data);
+      setIsError(false)
+      setLoading(false)
     } catch (isError) {
       setIsError(isError.message)
+      setLoading(false)
+
     }
   }
 useEffect(()=>{
@@ -20,10 +25,11 @@ apiData()
     <>
       <h1>Placeholder API</h1>
       {isError && <h1>ERROR:{isError}</h1> }
-        { 
+
+        { loading ? <h1>Loading...</h1> : (
           items.map((item)=>(
           <p key={item.id}> {item.title}</p>
-          ))
+          )))
         }
     </>
   )
