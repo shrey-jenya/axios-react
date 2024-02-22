e55a468ce49990859c2b6439ffe774e8e835ae4a
unknown <hardeepsinh.jenya@gmail.com>
2024-02-22 15:07:44 +0530

    meals api

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,42 @@
+import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import './App.css'
-import axios from 'axios'
 const App = () => {
-  const [items, setItems] = useState([])
+  const [meals, setMeals] = useState([])
+  const [loading, setLoading] = useState(true)
   const [isError, setIsError] = useState('')
-  const [loading,setLoading]=useState(true)
-  const apiData = async () => {
+  const mealsApi = async () => {
     try {
-      const res = await axios.get(`https://jsonplaceholder.typicode.com/todos/`)
-      setItems(res.data)
-      console.log(res.data);
-      setIsError(false)
+      const response = await axios.get('https://www.themealdb.com/api/json/v1/1/filter.php?a=American')
+      setMeals(response.data.meals)
       setLoading(false)
-    } catch (isError) {
-      setIsError(isError.message)
+      console.log(response.data.meals)
+    } catch (error) {
+      setIsError(error.message)
       setLoading(false)
 
     }
   }
-useEffect(()=>{
-apiData()
-},[])
+  useEffect(() => {
+    mealsApi()
+  }, [])
   return (
     <>
-      <h1>Placeholder API</h1>
-      {isError && <h1>ERROR:{isError}</h1> }
-
-        { loading ? <h1>Loading...</h1> : (
-          items.map((item)=>(
-          <p key={item.id}> {item.title}</p>
-          )))
-        }
+      <h1>Meals API - American</h1>
+      <input type="search" placeholder='search...' />
+      {isError !== '' && <h2>Error - {isError}</h2>}
+      <div className='grid'>
+        {
+          loading ? <h1>Loading...</h1> :
+            meals.map(({ idMeal, strMeal, strMealThumb }) => (
+              <div className='card' key={idMeal}>
+                <h4>{strMeal}</h4>
+                <img className='meals-img' src={strMealThumb} alt={strMeal} />
+              </div>
+            ))}
+      </div>
     </>
   )
 }
+
 export default App
