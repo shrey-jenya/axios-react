97b65e9d1ebf879ee68a65c77e7f67f7198c74b8
unknown <hardeepsinh.jenya@gmail.com>
2024-02-22 16:52:02 +0530

    search feture added in users

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-
 const App = () => {
   const [myData, setMyData] = useState([])
   const [isError, setIsError] = useState('')
-  const [loading , setIsLoading] = useState(true)
+  const [loading, setIsLoading] = useState(true)
+  const [searchQuery, setSearchQuery] = useState('')
   const apiData = async () => {
     try {
       const res = await axios.get('https://jsonplaceholder.typicode.com/users/')
@@ -12,32 +12,37 @@ const App = () => {
       setIsError(false)
       setIsLoading(false)
     } catch (error) {
-      setIsError(error.message  )
+      setIsError(error.message)
       setIsLoading(false)
-
     }
   }
   useEffect(() => {
     apiData()
   }, [])
+  const handleSearch = (e) => {
+    setSearchQuery(e.target.value)
+  }
+  const filterSearch = myData.filter((user) =>
+    user.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
   return (
     <div>
       <h1>PlaceHolder API - USERS</h1>
+      <input type="search" value={searchQuery} onChange={handleSearch} placeholder='Search User...' />
       {isError && <h1>{isError}</h1>}
       {
-        loading ? <h1>Loading...</h1> :(
-        myData.map(({ username, name, id, email, phone, website }) => (
-          <div className='parent' style={{ border: '1px solid #ccc', padding: '10px', margin: '10px', borderRadius: '5px', backgroundColor: '#f9f9f9' }} key={id}>
-            <h3 style={{ color: '#333', marginBottom: '5px' }}>{name}</h3>
-            <h4 style={{ color: '#666', marginBottom: '5px' }}>{username}</h4>
-            <p  style={{ color: '#888', marginBottom: '3px' }} >{email}</p>
-            <p  style={{ color: '#888', marginBottom: '3px' }}>{phone}</p>
-            <p  style={{ color: '#888', marginBottom: '3px' }}>{website}</p>
-          </div>
-        ))
-     ) }
+        loading ? <h1>Loading...</h1> : (
+          filterSearch.map(({ username, name, id, email, phone, website }) => (
+            <div className='parent' style={{ border: '1px solid #ccc', padding: '10px', margin: '10px', borderRadius: '5px', backgroundColor: '#f9f9f9' }} key={id}>
+              <h3 style={{ color: '#333', marginBottom: '5px' }}>{name}</h3>
+              <h4 style={{ color: '#666', marginBottom: '5px' }}>{username}</h4>
+              <p style={{ color: '#888', marginBottom: '3px' }} >{email}</p>
+              <p style={{ color: '#888', marginBottom: '3px' }}>{phone}</p>
+              <p style={{ color: '#888', marginBottom: '3px' }}>{website}</p>
+            </div>
+          ))
+        )}
     </div>
   )
 }
-
 export default App
